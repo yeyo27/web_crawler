c9c17469168707c10d30e0052f85ebbe74cf52e4
yeyo27 <yeyo1mm@gmail.com>
2023-08-20 12:48:46 +0200

    Added getURLsFromHTML an test

--- crawl.js
+++ crawl.js
@@ -1,11 +1,25 @@
 function normalizeURL(url) {
-    const urlObject =  new URL(url)
+    const urlObject =  new URL(url);
     if (url[url.length - 1] === "/") {
         urlObject.pathname = urlObject.pathname.slice(0, -1)
     }
     return `${urlObject.hostname}${urlObject.pathname}`
 }
 
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+function getURLsFromHTML(htmlBody, baseURL){
+    const dom = new JSDOM(htmlBody);
+    const anchorElements = dom.window.document.querySelectorAll('a')
+    let urls = []
+    anchorElements.forEach((anchorElement) => {
+        anchorElement.href[0] === '/' ? urls.push(baseURL + anchorElement.href) : urls.push(anchorElement.href)
+    })
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,6 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+const {getURLsFromHTML} = require("./crawl");
 
 test('normalize https://blog.boot.dev/path/ to equal blog.boot.dev/path', () => {
     expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
@@ -15,4 +16,15 @@ test('normalize http://blog.boot.dev/path/ to equal blog.boot.dev/path', () => {
 
 test('normalize http://blog.boot.dev/path to equal blog.boot.dev/path', () => {
     expect(normalizeURL('http://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+});
+
+const htmlString = `<html lang="en">
+    <body>
+        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        <a href="/path"><span>Go to Boot.dev/path</span></a>
+    </body>
+</html>`
+
+test('get url https://blog.boot.dev and https://blog.boot.dev/path from html', () => {
+    expect(getURLsFromHTML(htmlString, 'https://blog.boot.dev')).toStrictEqual(["https://blog.boot.dev/", "https://blog.boot.dev/path"])
 });
